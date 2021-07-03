9bbe36fba9756d5490447be4bf0423c0d5ad2af7
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-07-03 14:59:07 +0600

    Creating Context with Functional Components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import BookList from './components/BookList'
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle'
 import AuthContextProvider from './contexts/AuthContext';
+import BookContextProvider from './contexts/BookContext';
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <AuthContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+          <Navbar />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
+          <ThemeToggle />
         </AuthContextProvider>
       </ThemeContextProvider>
     </div>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,20 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
-
+import {BookContext} from '../contexts/BookContext'
 const BookList = () => {
     const { isLightTheme, light, dark } = useContext(ThemeContext)
     const theme = isLightTheme ? light : dark
+    const {books} =useContext(BookContext)
     return (
         <div className='book-list' style={{ color: theme.syntax, background: theme.bg }}>
             <ul>
-                <li style={{ background: theme.ui }}>the way of kings</li>
-                <li style={{ background: theme.ui }}>the name of the wind</li>
-                <li style={{ background: theme.ui }}>the final empire</li>
+                {
+                    books.map(book =>{
+                        return(
+                            <li style={{ background: theme.ui }} key={book.id}>{book.title}</li>
+                        )
+                    })
+                }
             </ul>
         </div>
     )
